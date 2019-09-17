8f272d7dfd93d381bfd664d04a1c7c873ed7a53f
niraliGandhi <niralibitto01@gmail.com>
2019-09-17 13:04:08 +0530

    dg
    g

--- contactForm/index.js
+++ contactForm/index.js
@@ -8,25 +8,21 @@ var app = express();
    password : '',
    database : 'certificate'
  });
- app.get('/', function (req, res) {
-    res.send('Hello World');
-  });
+ app.use(bodyParser.urlencoded({ extended: false }));
+//  app.get('/', function (req, res) {
+//     res.send('Hello World');
+//   });
   app.use(express.static('public'));
   app.listen(3000);
-  connection.connect(function(err){
-    if(!err) {
-        console.log("Database is connected ... \n\n");  
-    } else {
-        console.log("Error connecting database ... \n\n");  
-    }
-    });
-   
-
-//  connection.query('SELECT * from < table name >', function(err, rows, fields) {
-//    if (!err)
-//      console.log('The solution is: ', rows);
-//    else
-//      console.log('Error while performing Query.');
-//  });
-
- connection.end();
+  app.post('/submit-form', (req, res) => {
+    const certificate_no = req.body.certificate_no;
+    console.log(certificate_no);
+  })
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  con.query("SELECT * FROM MainTable where Certificate_number="+certificate_no, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
